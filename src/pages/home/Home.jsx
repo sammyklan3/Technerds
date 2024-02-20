058d67e719eedc2004ad3bc543eba7083e848212
sammyklan3 <sammyklane7@gmail.com>
2024-02-20 16:21:37 +0300

    Minor changes

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,17 +3,34 @@ import { FiSettings } from "react-icons/fi";
 import { FiSearch } from "react-icons/fi";
 
 export const Home = () => {
-  return (
-    <div className="events-home-container">
-      <div className="events-title">
-        <h2>Events</h2>
-        <p><FiSettings /></p>
-      </div>
+    return (
+        // Root div
+        <div className="events-home-container">
+            {/* Events title */}
+            <div className="events-title">
+                <h2>Events</h2>
+                <p><FiSettings /></p>
+            </div>
+            {/* Search container */}
+            <div className="search-container">
+                <FiSearch />
+                <input type="search" placeholder="Search events" />
+            </div>
 
-      <div className="search-container">
-        <FiSearch />
-        <input type="search" placeholder="Search events"/>
-      </div>
-    </div>
-  )
+            {/* Time selector filter */}
+            <div className="time-selector">
+                <ul>
+                    <li>
+                        <p>All</p>
+                    </li>
+                    <li>
+                        <p>Today</p>
+                    </li>
+                    <li>
+                        <p>Tomorrow</p>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    )
 }
