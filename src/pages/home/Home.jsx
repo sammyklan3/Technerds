d495f3a33d1fb2a55ab1d7cf49cac6a8944403ba
sammyklan3 <sammyklane7@gmail.com>
2024-02-20 17:43:29 +0300

    Minor changes

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,17 @@
 import "./home.css";
 import { FiSettings } from "react-icons/fi";
 import { FiSearch } from "react-icons/fi";
+import { useState } from "react";
 
 export const Home = () => {
+
+    // State variables to hold the values of input fields
+    const [textInput, setTextInput] = useState('');
+
+    // Event handlers to update state when input values change
+    const handleTextInputChange = (event) => {
+        setTextInput(event.target.value);
+    };
     return (
         // Root div
         <div className="events-home-container">
@@ -14,7 +23,8 @@ export const Home = () => {
             {/* Search container */}
             <div className="search-container">
                 <FiSearch />
-                <input type="search" placeholder="Search events" />
+                <input type="search" placeholder="Search events" value={textInput}
+                    onChange={handleTextInputChange} />
             </div>
 
             {/* Time selector filter */}
@@ -28,6 +38,28 @@ export const Home = () => {
                     </li>
                     <li>
                         <p>Tomorrow</p>
+                    </li>
+                    <li>
+                        <p>This weekend</p>
+                    </li>
+                </ul>
+            </div>
+
+            {/* Main content */}
+            <div className="events-main-container">
+                <ul>
+                    <li className="events-main-item">
+                        <img src="https://ioflood.com/blog/wp-content/uploads/2023/09/Retrieving-current-directory-in-Python-folder-path-directory-tree-code-logo.jpg" alt="events-main-item-image" />
+                        <div className="events-main-item-details">
+                            <h3>Introduction to Python</h3>
+                            <p>Fri, Dec 2023, 2:00 PM</p>
+                            <div className="events-main-item-details-actions">
+                                <p>1,100 going . 2,000 interested</p>
+                                <button type="button" className="events-main-item-details-actionsBtn">
+                                    <p>Interested</p>
+                                </button>
+                            </div>
+                        </div>
                     </li>
                 </ul>
             </div>
