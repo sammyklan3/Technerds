e2a8fce6745177efd7ba7dd8aa4fb66ce0a1fbb5
sammyklan3 <sammyklane7@gmail.com>
2024-02-20 16:03:11 +0300

    Adding home page

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Home } from './pages/home/Home';
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import './index.css';
+
+const router = createBrowserRouter([
+  {
+  path: "/",
+  element: <Home />
+  }
+
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
